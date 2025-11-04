69255ce78297af470e61989b488fc07cb0f22998
Raman Viswanathan <raman22feb1988@gmail.com>
2025-11-04 18:14:34 +0530

    Enhancements for #29.
    Enhancements for #29.

--- app/src/main/java/com/tuchwords/wordstudy/sqliteDB.java
+++ app/src/main/java/com/tuchwords/wordstudy/sqliteDB.java
@@ -61,22 +61,22 @@ public class sqliteDB extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         // TODO Auto-generated method stub
         db.execSQL(
-                "create table words(_word_ text collate nocase primary key, _length_ integer, _alphagram_ text collate nocase, _definition_ text collate nocase, _probability_ real, _back_ text collate nocase, _front_ text collate nocase, _tag_ text collate nocase, _page_ integer, _answers_ integer, _csw24_ integer, _csw21_ integer, _csw19_ integer, _csw15_ integer, _csw12_ integer, _csw07_ integer, _nwl23_ integer, _nwl20_ integer, _nwl18_ integer, _twl06_ integer, _nswl23_ integer, _nswl20_ integer, _nswl18_ integer, _wims_ integer, _cel21_ integer, _serial_ integer, _position_ integer, _timestamp_ text collate nocase, _reverse_ text collate nocase, _zetagram_ text collate nocase, _no_a_ integer, _no_b_ integer, _no_c_ integer, _no_d_ integer, _no_e_ integer, _no_f_ integer, _no_g_ integer, _no_h_ integer, _no_i_ integer, _no_j_ integer, _no_k_ integer, _no_l_ integer, _no_m_ integer, _no_n_ integer, _no_o_ integer, _no_p_ integer, _no_q_ integer, _no_r_ integer, _no_s_ integer, _no_t_ integer, _no_u_ integer, _no_v_ integer, _no_w_ integer, _no_x_ integer, _no_y_ integer, _no_z_ integer, _vowels_ integer, _consonants_ integer, _points_ integer, _power_ integer)"
+                "create table if not exists words(_word_ text collate nocase primary key, _length_ integer, _alphagram_ text collate nocase, _definition_ text collate nocase, _probability_ real, _back_ text collate nocase, _front_ text collate nocase, _tag_ text collate nocase, _page_ integer, _answers_ integer, _csw24_ integer, _csw21_ integer, _csw19_ integer, _csw15_ integer, _csw12_ integer, _csw07_ integer, _nwl23_ integer, _nwl20_ integer, _nwl18_ integer, _twl06_ integer, _nswl23_ integer, _nswl20_ integer, _nswl18_ integer, _wims_ integer, _cel21_ integer, _serial_ integer, _position_ integer, _timestamp_ text collate nocase, _reverse_ text collate nocase, _zetagram_ text collate nocase, _no_a_ integer, _no_b_ integer, _no_c_ integer, _no_d_ integer, _no_e_ integer, _no_f_ integer, _no_g_ integer, _no_h_ integer, _no_i_ integer, _no_j_ integer, _no_k_ integer, _no_l_ integer, _no_m_ integer, _no_n_ integer, _no_o_ integer, _no_p_ integer, _no_q_ integer, _no_r_ integer, _no_s_ integer, _no_t_ integer, _no_u_ integer, _no_v_ integer, _no_w_ integer, _no_x_ integer, _no_y_ integer, _no_z_ integer, _vowels_ integer, _consonants_ integer, _points_ integer, _power_ integer)"
         );
         db.execSQL(
-                "create table scores(_length_ integer, _query_ text collate nocase, _counter_ integer, _sort_ text collate nocase)"
+                "create table if not exists scores(_length_ integer, _query_ text collate nocase, _counter_ integer, _sort_ text collate nocase)"
         );
         db.execSQL(
-                "create table colours(_tag_ text collate nocase, _colour_ text collate nocase)"
+                "create table if not exists colours(_tag_ text collate nocase, _colour_ text collate nocase)"
         );
         db.execSQL(
-                "create table zoom(_activity_ text collate nocase, _rows_ integer, _columns_ integer, _size_ integer)"
+                "create table if not exists zoom(_activity_ text collate nocase, _rows_ integer, _columns_ integer, _size_ integer)"
         );
         db.execSQL(
-                "create table prefixes(_prefix_ text collate nocase, _before_ text collate nocase)"
+                "create table if not exists prefixes(_prefix_ text collate nocase, _before_ text collate nocase)"
         );
         db.execSQL(
-                "create table suffixes(_suffix_ text collate nocase, _after_ text collate nocase)"
+                "create table if not exists suffixes(_suffix_ text collate nocase, _after_ text collate nocase)"
         );
     }
 
